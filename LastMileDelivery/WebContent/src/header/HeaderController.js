dff4d92adb20392938b63a4174427391a66ee04f
ChandravadhanaTK <tkchandravadhana@gmail.com>
2018-08-03 11:02:45 +0530

    AngularUI

---
+++ LastMileDelivery/WebContent/src/header/HeaderController.js
@@ -0,0 +1,11 @@
+﻿/*Header Controller*/
+(function () {
+    'use strict';
+    var HeaderController = function ($scope,$rootScope,$location) {
+        
+    }
+
+    HeaderController.$inject = ['$scope','$rootScope','$location'];
+
+    angular.module('app.MYApp').controller('HeaderController', HeaderController);
+}());
